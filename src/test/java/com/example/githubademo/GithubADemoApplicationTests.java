52c448895afe4e7ffeb78bfe5072af3a2f8c8b1a
carobraaa <166133374+carobraaa@users.noreply.github.com>
2024-09-24 10:11:32 +0200

    Initial commit

---
+++ src/test/java/com/example/githubademo/GithubADemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubADemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
